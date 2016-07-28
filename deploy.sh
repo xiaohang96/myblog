b2e2af4bcf9e5a8ded1490727388217787b841da
xiaohang <335676008@qq.com>
2016-07-28 16:41:27 +0800

    完整相册功能实现

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 hexo generate
 cp -R public/* .deploy/xiaohang96.github.io
-cd .deploy/xiaohang96.github.io
+cd .deploy/jiji262.github.io
 git add .
 git commit -m ¡°update¡±
 git push origin master
